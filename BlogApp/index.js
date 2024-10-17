6039a6bfe87525d9b3368a87232920367ce5095a
RahulPraj <rahul971801@gmail.com>
2024-10-17 23:11:06 +0530

    final

--- BlogApp/index.js
+++ BlogApp/index.js
@@ -3,9 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Blog = require('./models/blog');
-
-
-
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://127.0.0.1:27017/blogg')
 .then(()=>{console.log('Server connected')})
@@ -15,7 +13,7 @@ app.set('view engine' , 'ejs');
 app.set('views' , path.join(__dirname , 'views'));
 app.use(express.static(path.join(__dirname , 'public')));
 app.use(express.urlencoded({ extended: true }))  //body parsing middleware -> formdata
-
+app.use(methodOverride('_method'))
 
 //read
 app.get('/blogs' , async(req,res)=>{
@@ -37,10 +35,35 @@ app.post('/blogs' ,async(req,res)=>{
     res.redirect('/blogs')
  })
 
+ //show a particular blog
  app.get('/blogs/:id' , async(req,res)=>{
     let {id} = req.params;
     let foundProduct = await Blog.findById(id);
     res.render('show' , {foundProduct})
+})
+
+//edit
+app.get('/blogs/:idd/edit' , async(req,res)=>{
+    let {idd} = req.params;
+    let foundProduct =  await Blog.findById(idd);
+    res.render('edit' , {foundProduct});
+})
+
+//update a blog
+app.patch('/blogs/:id' , async(req,res)=>{
+    let {id} =  req.params;
+    // console.log(req.params.id);
+    let {comment} = req.body;
+    // console.log(req.body.comment)
+    await Blog.findByIdAndUpdate(id , {comment});
+    res.redirect('/Blogs')
+})
+
+// deleting
+app.delete('/blogs/:id' , async(req,res)=>{
+    let {id} =  req.params;
+    await Blog.findByIdAndDelete(id);
+    res.redirect('/blogs')
 })
 
 app.listen(8080,()=>{
